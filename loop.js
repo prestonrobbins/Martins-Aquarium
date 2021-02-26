7a1f5a676bfc07073696bb2e10261dadec54cf7d
prestonshotts <shotts.preston@gmail.com>
2021-02-25 19:14:36 -0600

    first commit

---
+++ loop.js
@@ -0,0 +1,14 @@
+// import {fishList} from "./list.js";
+
+
+
+// const fishListExport = () => {
+//     console.log(fishList())
+// }
+
+
+// export const fishLoopTry = () => {
+// for (const fishLoop of fishList) {
+//     console.log(fish)
+// }
+// }
